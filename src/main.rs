474a698840b1489590cec7c3d275f4135b3f8cb0
Chris Kaltwasser <chris@chrisk.com>
2021-12-23 12:00:46 -0500

    comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// The current version, shown below, uses the daemon function to cause
+// the current program to run as a child process. The parent process
+// apparently then terminates. The surviving process then continues to run
+// and proceeds to append lines to the file named "fork.log" in the 
+// current directory. The first arg to the daemon function, named `nochdir`,
+// is set to true, otherwise the file io would target / and most likely
+// fail if not run with root authority.
+//
+// The process id (pid) for both the parent is displayed at startup.
+// The child process pids are included in the "fork.log" output.
+// Testing showed this process will survive logging out from a Gnome Desktop
+// session. Issuing a bash kill without params will terminate it.
+
 use fork::{Fork, daemon};
 use std::{thread, time, process};
 use std::io::Write;
